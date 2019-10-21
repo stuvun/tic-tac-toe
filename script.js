9175c5227c9fc1d77a7d89b7da5c999e74686a01
Steven Kim <sskim14@gmail.com>
2019-10-21 15:02:15 -0400

    Planned functions

---
+++ script.js
@@ -0,0 +1,27 @@
+// Game starts
+
+// Display turn phase (game starts with Player 1)
+// Player 1 selects a box
+// The box that Player 1 picks turns red
+// The box becomes unclickable
+// CHECK IF THERE'S A WINNER
+
+// Display turn phase Player 2
+// Player 2 selects a box
+// The box that Player 2 picks turns blue
+// The box becomes unclickable
+// CHECK IF THERE'S A WINNER
+
+// | If there are three reds in a line, Player 1 wins   |
+// | Add a point to Player 1's score                    |   CHECK
+// | If there are three blues in a line, Player 2 wins  |   IF
+// | Add a point to Player 2's score                    |   THERE'S
+// | If all boxes are unclickable && there is no winner,|   A
+// | The game ends in a tie.                            |   WINNER
+// | Show reset button                                  |
+
+// If reset button is clicked, turn box colors back to white
+// Make boxes clickable again
+// Game starts
+
+
