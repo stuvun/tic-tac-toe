17829284e48fb24fb1adf81e452d31c178d071d0
Steven <sskim14@gmail.com>
2019-10-27 22:36:33 -0400

    Added condition for draw

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-// Game starts
-
-// Display turn phase (game starts with Player 1)
-// Player 1 selects a box
-// The box that Player 1 picks turns red
-// The box becomes unclickable
-// CHECK IF THERE'S A WINNER
-
-// Display turn phase Player 2
-// Player 2 selects a box
-// The box that Player 2 picks turns blue
-// The box becomes unclickable
-// CHECK IF THERE'S A WINNER
-
-// | If there are three reds in a line, Player 1 wins       |
-// | Add a point to Player 1's score                        |   CHECK
-// | If there are three blues in a line, Player 2 wins      |   IF
-// | Add a point to Player 2's score                        |   THERE'S
-// | If all boxes are unclickable && there is no winner,    |   A
-// | The game ends in a tie.                                |   WINNER
-// | Show reset button                                      |
-
-// If reset button is clicked, turn box colors back to white
-// Make boxes clickable again
-// Game starts
-
 const game = document.querySelectorAll(".item");
 const win1 = document.querySelector(".player1Win");
 const win2 = document.querySelector(".player2Win");
+const draw = document.querySelector(".gameDraw");
 
 let turn = 0;
 let red = "redredred";
 let blue = "blueblueblue";
+let round = 0;
 
-function endWin1() {
-    turn = 3;
-    win1.classList.toggle("hidden")
-}
+function endWin1() { turn = 3; win1.classList.toggle("hidden") }
+function endWin2() { turn = 3; win2.classList.toggle("hidden") }
 
-function endWin2() {
-    turn = 3;
-    win2.classList.toggle("hidden")
+function checkTie() {
+    if ( round == 9 ) {
+        if (win1.classList.contains("hidden") == true && win2.classList.contains("hidden") == true) {
+            draw.classList.toggle("hidden");
+            turn = 4
+        }
+    } else { return round }
 }
 
 game.forEach(function(block) {
+
     block.addEventListener("click", function(evt) {
         evt.preventDefault();
-        
-        console.log(block);
 
         if (turn == 0 && block.style.background == "") {
             block.style.background = "red";
@@ -83,7 +60,8 @@ game.forEach(function(block) {
                     + game[4].style.background
                     + game[6].style.background
                    == red ) { endWin1() }
-        } else if (turn == 1 && block.style.background == "") {
+        
+ } else if (turn == 1 && block.style.background == "") {
             block.style.background = "blue";
             turn = 0;
             if      ( game[0].style.background
@@ -119,5 +97,7 @@ game.forEach(function(block) {
                     + game[6].style.background
                    == blue ) { endWin2() }
         }
+        round = round + 1;
+        checkTie()
     })
 })
