2601ca2c29651e509b2f883fef92ffcb0d20e36c
Steven <sskim14@gmail.com>
2019-10-28 00:58:28 -0400

    Game is fully functional

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const play1Turn = document.querySelector(".player1");
 const play2Turn = document.querySelector(".player2");
+
 const game = document.querySelectorAll(".item");
+const score1 = document.querySelector(".score1");
+const score2 = document.querySelector(".score2");
+
 const win1 = document.querySelector(".player1Win");
 const win2 = document.querySelector(".player2Win");
 const draw = document.querySelector(".gameDraw");
-const score1 = document.querySelector(".score1");
-const score2 = document.querySelector(".score2");
+const rematch = document.querySelector(".rematch");
 
 let turn = 0;
 let red = "redredred";
@@ -17,23 +20,43 @@ let player2 = 0;
 function endWin1() {
     turn = 3;
     win1.classList.toggle("hidden");
-    play1Turn.classList.remove("hidden"); player1 = player1 + 1;
-    score1.innerHTML = "Player 1: " + player1
+    play1Turn.classList.add("hidden");
+    play2Turn.classList.add("hidden");
+    player1 = player1 + 1;
+    score1.innerHTML = "Player 1: " + player1;
+    rematch.classList.toggle("hidden")
 }
 function endWin2() {
     turn = 3; win2.classList.toggle("hidden");
-    play2Turn.classList.remove("hidden"); player2 = player2 + 1;
-    score2.innerHTML = "Player 2: " + player2
+    play1Turn.classList.add("hidden");
+    play2Turn.classList.add("hidden");
+    player2 = player2 + 1;
+    score2.innerHTML = "Player 2: " + player2;
+    rematch.classList.toggle("hidden")
 }
 
 function checkTie() {
     if ( round == 9 ) {
         if (win1.classList.contains("hidden") == true && win2.classList.contains("hidden") == true) {
             draw.classList.toggle("hidden");
-            turn = 4
+            turn = 4;
+            rematch.classList.toggle("hidden")
         }
     } else { return round }
 }
+
+rematch.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    game.forEach(function(clear) {
+        clear.style.background = "";
+        win1.classList.add("hidden");
+        win2.classList.add("hidden");
+        turn = 0;
+        round = 0;
+        play1Turn.classList.remove("hidden");
+        rematch.classList.toggle("hidden")
+    })
+})
 
 game.forEach(function(block) {
 
