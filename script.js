c8b6ea869ee7e08014271c2217a4f8484b5de2fe
Steven <sskim14@gmail.com>
2019-10-28 00:33:02 -0400

    Game runs normally. Restart function not yet created

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
+const play1Turn = document.querySelector(".player1");
+const play2Turn = document.querySelector(".player2");
 const game = document.querySelectorAll(".item");
 const win1 = document.querySelector(".player1Win");
 const win2 = document.querySelector(".player2Win");
 const draw = document.querySelector(".gameDraw");
+const score1 = document.querySelector(".score1");
+const score2 = document.querySelector(".score2");
 
 let turn = 0;
 let red = "redredred";
 let blue = "blueblueblue";
 let round = 0;
+let player1 = 0;
+let player2 = 0;
 
-function endWin1() { turn = 3; win1.classList.toggle("hidden") }
-function endWin2() { turn = 3; win2.classList.toggle("hidden") }
+function endWin1() {
+    turn = 3;
+    win1.classList.toggle("hidden");
+    play1Turn.classList.remove("hidden"); player1 = player1 + 1;
+    score1.innerHTML = "Player 1: " + player1
+}
+function endWin2() {
+    turn = 3; win2.classList.toggle("hidden");
+    play2Turn.classList.remove("hidden"); player2 = player2 + 1;
+    score2.innerHTML = "Player 2: " + player2
+}
 
 function checkTie() {
     if ( round == 9 ) {
@@ -24,10 +39,15 @@ game.forEach(function(block) {
 
     block.addEventListener("click", function(evt) {
         evt.preventDefault();
+        let player1 = play1Turn.classList.toggle("hidden");
+        let player2 = play2Turn.classList.toggle("hidden");
 
         if (turn == 0 && block.style.background == "") {
             block.style.background = "red";
             turn = 1;
+            player1;
+            player2;
+
             if      ( game[0].style.background
                     + game[1].style.background
                     + game[2].style.background
@@ -64,6 +84,9 @@ game.forEach(function(block) {
  } else if (turn == 1 && block.style.background == "") {
             block.style.background = "blue";
             turn = 0;
+            player2;
+            player1;
+
             if      ( game[0].style.background
                     + game[1].style.background
                     + game[2].style.background
